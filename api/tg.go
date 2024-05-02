b9dd74eca22009bc7586d2c1725a30da23b7a1c6
qiaoliangXgamemode <pppwwi@qq.com>
2024-05-02 21:47:25 +0800

    Changes to be committed: 	modified:   api/tg.go

--- api/tg.go
+++ api/tg.go
@@ -1,9 +1,8 @@
 package api
 
 import (
-	"fmt"
+	"Myblog/Parser"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,30 +13,7 @@ var apiUrl = "https://api.telegram.org"
 
 func init() {
 	router = gin.Default()
-	router.Any("/*path", func(context *gin.Context) {
-		uri := context.Param("path")
-		if !strings.Contains(uri, "bot") {
-			context.String(http.StatusNotFound, "404 Not found")
-			return
-		}
-		url := apiUrl + uri
-		req, err := http.NewRequestWithContext(context, context.Request.Method, url, context.Request.Body)
-		if err != nil {
-			fmt.Println(err)
-			context.String(http.StatusBadRequest, err.Error())
-			return
-		}
-		req.Header = context.Request.Header
-		req.PostForm = context.Request.PostForm
-		req.Form = context.Request.Form
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			context.String(http.StatusBadRequest, err.Error())
-			return
-		}
-		context.DataFromReader(resp.StatusCode, resp.ContentLength, "application/json", resp.Body, nil)
-	})
+	router.Any("/", Parser.ListResponseJSON)
 }
 
 func Listen(w http.ResponseWriter, r *http.Request) {
