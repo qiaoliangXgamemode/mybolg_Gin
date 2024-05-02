7db85f7a6ac4a5e4e1751204c2d34e8f0f66ad17
qiaoliangXgamemode <pppwwi@qq.com>
2024-05-02 21:20:18 +0800

    Changes to be committed: 	modified:   api/route.go

--- api/route.go
+++ api/route.go
@@ -25,7 +25,7 @@ func init() {
 	// r.GET("/", func(context *gin.Context) {
 	// 	context.String(http.StatusOK, "hellow")
 	// })
-	// r.GET("/", Parser.ListResponseJSON)
+	r.GET("/", Parser.ListResponseJSON)
 	r.GET("/ArticleList", Parser.ListResponseJSON)
 
 	r.GET("/articleContext/:title", Parser.ParserMarkdown)
